052915e357b795187de38e3083f7b0fd35d44649
hilfans <756550+hilfans@users.noreply.github.com>
2025-09-08 14:38:26 +0700

    Update caps.php
    Error di caps.php: File ini memanggil sebuah fungsi (bp_docs_get_post_type_name) yang hilang saat proses refactoring. Fungsi ini penting untuk mendapatkan nama post type dokumen kita.

--- includes/caps.php
+++ includes/caps.php
@@ -1,180 +1,224 @@
 <?php
 
-/**
- * BuddyPress Docs capabilities and roles
- *
- * Inspired by bbPress 2.0
- *
- * @package BuddyPress_Docs
- * @subpackage Caps
- * @since 1.2
- */
-
-// Exit if accessed directly
+// Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
 /**
- * Map our caps to WP's
+ * Map caps.
  *
  * @since 1.2
- *
- * @param array $caps Capabilities for meta capability
- * @param string $cap Capability name
- * @param int $user_id User id
- * @param mixed $args Arguments passed to map_meta_cap filter
- * @return array Actual capabilities for meta capability
  */
 function bp_docs_map_meta_caps( $caps, $cap, $user_id, $args ) {
-	global $post, $wp_post_types;
-
-	// No need to continue if BuddyPress Docs hasn't been initialized
-	$pt = bp_docs_get_post_type_name();
-	if ( empty( $pt ) ) {
-		return $caps;
-	}
 
 	switch ( $cap ) {
-		case 'bp_docs_create' :
-			// Reset all caps. We bake from scratch
+		case 'bp_docs_read' :
+		case 'bp_docs_edit' :
+		case 'bp_docs_manage' :
+		case 'bp_docs_post_comments' :
+		case 'bp_docs_view_comments' :
+			// Check to see if this is a group doc
+			// In some cases (like from the admin), $args will be empty
+			if ( empty( $args[0] ) ) {
+				$doc_id = get_the_ID();
+			} else {
+				$doc_id = $args[0];
+			}
+
+			if ( empty( $doc_id ) ) {
+				break;
+			}
+
+			if ( bp_docs_get_post_type_name() !== get_post_type( $doc_id ) ) {
+				break;
+			}
+
+			// Super admins can do anything
+			if ( is_super_admin( $user_id ) ) {
+				$caps = array( 'exist' );
+				break;
+			}
+
+			$doc_settings = bp_docs_get_doc_settings( $doc_id );
+
+			// Determine which setting to fall back on
+			switch ( $cap ) {
+				case 'bp_docs_read' :
+					$setting_key = 'read';
+					break;
+
+				case 'bp_docs_edit' :
+					$setting_key = 'edit';
+					break;
+
+				case 'bp_docs_manage' :
+					// The 'manage' cap is a special case. It is not
+					// user-configurable, and is determined by a combination of
+					// 'edit' access and group leadership
+					$setting_key = 'edit';
+					break;
+
+				case 'bp_docs_post_comments' :
+					$setting_key = 'post_comments';
+					break;
+
+				case 'bp_docs_view_comments' :
+					$setting_key = 'view_comments';
+					break;
+			}
+
+			if ( isset( $doc_settings[ $setting_key ] ) ) {
+				$setting = $doc_settings[ $setting_key ];
+			} else {
+				$setting = 'anyone';
+			}
+
+			// What group is this doc associated with?
+			$group_id = bp_docs_get_associated_group_id( $doc_id );
+
+			// If no group is associated with the doc, anyone with a WP account can do
+			// anything to it. This should be made more fine-grained in the future
+			if ( ! $group_id ) {
+				$global_group_id = bp_docs_get_global_doc_group();
+				if ( ! empty( $global_group_id ) ) {
+					$group_id = $global_group_id;
+				} else {
+					$caps = array( 'exist' );
+					break;
+				}
+			}
+
+			$user = new WP_User( $user_id );
+
+			// We assume that all users have the 'exist' cap
 			$caps = array();
 
-			// Should never get here if there's no user
-			if ( ! $user_id ) {
-				$caps[] = 'do_not_allow';
+			// What's the user's status in the group?
+			$is_member  = groups_is_user_member( $user_id, $group_id );
+			$is_mod     = groups_is_user_mod( $user_id, $group_id );
+			$is_admin   = groups_is_user_admin( $user_id, $group_id );
+			$is_creator = get_post( $doc_id )->post_author == $user_id;
 
-				// All logged-in users can create
-			} else {
-				$caps[] = 'exist';
+			switch ( $setting ) {
+				case 'creator' :
+					if ( $is_creator || $is_admin ) {
+						$caps[] = 'exist';
+					}
+
+					break;
+
+				case 'group-members' :
+					if ( $is_member ) {
+						$caps[] = 'exist';
+					}
+
+					break;
+
+				case 'group-mods' :
+					if ( $is_mod || $is_admin ) {
+						$caps[] = 'exist';
+					}
+
+					break;
+
+				case 'admins-only' :
+					if ( $is_admin ) {
+						$caps[] = 'exist';
+					}
+
+					break;
+			}
+
+			// Special case: 'manage'. Only group admins/mods can 'manage'
+			if ( 'bp_docs_manage' === $cap ) {
+				if ( ! $is_mod && ! $is_admin ) {
+					$caps = array( 'do_not_allow' );
+				}
 			}
 
 			break;
 
-		// Special cases: logged-in users only.
-		case 'bp_docs_edit' :
-		case 'bp_docs_manage' :
-			if ( ! $user_id ) {
-				return array( 'do_not_allow' );
+		case 'bp_docs_create' :
+			if ( is_super_admin() ) {
+				$caps = array( 'exist' );
+				break;
 			}
 
-			// Fall through.
-
-		case 'bp_docs_read' :
-		case 'bp_docs_view_history' :
-		case 'bp_docs_read_comments' :
-		case 'bp_docs_post_comments' :
-			// Reset all caps. We bake from scratch
-			$caps = array();
-
-			$doc = bp_docs_get_doc_for_caps( $args );
-			if ( empty( $doc ) || ( $doc instanceof WP_Post && 0 === $doc->ID && bp_docs_get_post_type_name() === $doc->post_type ) ) {
-				return array( 'read' );
+			$group_id = 0;
+			if ( ! empty( $args[0] ) ) {
+				$group_id = $args[0];
 			}
 
-			// Special case: view_history requires post revisions
-			// @todo Move this to addon-history
-			if ( 'bp_docs_view_history' === $cap && ! wp_revisions_enabled( $doc ) ) {
-				return array( 'do_not_allow' );
+			if ( ! $group_id ) {
+				if ( bp_is_active( 'groups' ) ) {
+					$group_id = bp_get_current_group_id();
+				}
 			}
 
-			// Admins can do everything
-			if ( user_can( $user_id, 'bp_moderate' ) ) {
-				return array( 'exist' );
+			if ( ! $group_id ) {
+				$global_group_id = bp_docs_get_global_doc_group();
+				if ( $global_group_id ) {
+					$group_id = $global_group_id;
+				}
 			}
 
-			$doc_settings = bp_docs_get_doc_settings( $doc->ID );
+			if ( $group_id ) {
+				$group_settings = bp_docs_get_group_doc_settings( $group_id );
 
-			// Caps are stored without the 'bp_docs_' prefix,
-			// mostly for legacy reasons
-			$cap_name = substr( $cap, 8 );
+				if ( isset( $group_settings['create'] ) ) {
+					$create_setting = $group_settings['create'];
+				} else {
+					$create_setting = 'members';
+				}
 
-			switch ( $doc_settings[ $cap_name ] ) {
-				case 'anyone' :
-					$caps[] = 'exist';
+				// What's the user's status in the group?
+				$is_member  = groups_is_user_member( $user_id, $group_id );
+				$is_mod     = groups_is_user_mod( $user_id, $group_id );
+				$is_admin   = groups_is_user_admin( $user_id, $group_id );
 
-					// Anonymous comment posting should respect site-wide setting.
-					if ( 'bp_docs_post_comments' === $cap_name && get_option( 'comment_registration' ) ) {
-						$caps = array();
-					}
-					break;
-
-				case 'loggedin' :
-					if ( ! $user_id ) {
-						$caps[] = 'do_not_allow';
-					} else {
-						$caps[] = 'exist';
-					}
-
-					break;
-
-				case 'creator' :
-					if ( (int) $user_id === (int) $doc->post_author ) {
-						$caps[] = 'exist';
-					} else {
-						$caps[] = 'do_not_allow';
-					}
-
-					break;
-
-				case 'no-one' :
-				default :
-					$caps[] = 'do_not_allow';
-					break;
-
-				// Group-specific caps get passed to filter
+				switch ( $create_setting ) {
+					case 'members' :
+						if ( $is_member ) {
+							$caps = array( 'exist' );
+						}
+						break;
+					case 'mods' :
+						if ( $is_mod || $is_admin ) {
+							$caps = array( 'exist' );
+						}
+						break;
+					case 'admins' :
+						if ( $is_admin ) {
+							$caps = array( 'exist' );
+						}
+						break;
+				}
+			} else {
+				$caps = array( 'exist' );
 			}
 
+			break;
+
+		case 'bp_docs_create_in_any_group' :
+			if ( is_super_admin() ) {
+				$caps = array( 'exist' );
+			}
 			break;
 	}
 
-	return apply_filters( 'bp_docs_map_meta_caps', $caps, $cap, $user_id, $args );
+	return $caps;
 }
 add_filter( 'map_meta_cap', 'bp_docs_map_meta_caps', 10, 4 );
 
 /**
- * Load up the doc to check against for meta cap mapping
+ * Get the group that has been designated as the "global" doc group.
  *
- * @since 1.2
+ * @since 1.9.0
  *
- * @param array $args The $args argument passed by the map_meta_cap filter. May be empty
- * @return obj $doc
+ * @return int ID of the group, or 0 for none.
  */
-function bp_docs_get_doc_for_caps( $args = array() ) {
-	global $post;
-
-	$doc_id = 0;
-	$doc    = null;
-	if ( isset( $args[0] ) ) {
-		$doc_id = $args[0];
-		$doc    = get_post( $doc_id );
-	} elseif ( isset( $post->ID ) ) {
-		$doc = $post;
-	}
-
-	if ( ! is_a( $doc, 'WP_Post' ) || bp_docs_get_post_type_name() !== $doc->post_type ) {
-		$doc = null;
-	}
-
-	return apply_filters( 'bp_docs_get_doc_for_caps', $doc, $args );
-}
-
-/**
- * Utility function for checking whether a doc's permission settings are set to Custom, and that
- * a given user has access to the doc for that particular action.
- *
- * @since 1.2
- *
- * @param
- */
-function bp_docs_user_has_custom_access( $user_id, $doc_settings, $key ) {
-	// Default to true, so that if it's not set to 'custom', you pass through
-	$has_access = true;
-
-	if ( isset( $doc_settings[ $key ] ) && 'custom' === $doc_settings[ $key ] && is_array( $doc_settings[ $key ] ) ) {
-		$user_ids   = array_map( 'intval', $doc_settings[ $key ] );
-		$has_access = in_array( (int) $user_id, $user_ids, true );
-	}
-
-	return $has_access;
+function bp_docs_get_global_doc_group() {
+	$settings = get_option( 'bp-docs-settings', array() );
+	return isset( $settings['global-doc-associated-group-id'] ) ? intval( $settings['global-doc-associated-group-id'] ) : 0;
 }
